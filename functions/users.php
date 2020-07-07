295fb8be1240867c8ca5d5cf1427e54ce3bd6b79
OzanYDZ51 <ozan.y@outlook.fr>
2020-07-07 17:08:49 +0200

    màj

---
+++ functions/users.php
@@ -0,0 +1,29 @@
+<?php
+
+class Users {
+    public $id;
+    public $email;
+    public $lastName;
+    public $firstName;
+    public $password;
+    public $birthday;
+    public $create_at;
+
+    public function __construct($id, $email, $lastName, $firstName, $password, $birthday, $create_at)
+    {
+        $this->id = $id;
+        $this->email = $email;
+        $this->lastname = $lastName;
+        $this->firstName = $firstName;
+        $this->passowrd = $password;
+        $this->birthday = $birthday;
+        $this->create_at = $create_at;
+    }
+
+    public function inscription($email, $lastName, )
+
+    public function verificationMail($id, $email) {
+        if ($_POST['email'] )
+    }
+
+}
